c43dd233646a49720e23d2a706fe29a387e52e67
Matteo Gioioiso <madeo@ip-192-168-1-3.ap-southeast-1.compute.internal>
2020-05-08 16:48:47 +0700

    add Poll and Remove method

--- priorityQueue/main.go
+++ priorityQueue/main.go
@@ -20,7 +20,7 @@ func (b *BinaryHeap) getParentIndexFromRightChild(rightChildIndex int) int {
 	return (rightChildIndex - 2) / 2
 }
 
-func (b BinaryHeap) isRightChild(index int) bool {
+func (b *BinaryHeap) isRightChild(index int) bool {
 	if index%2 == 0 {
 		return true
 	}
@@ -28,7 +28,7 @@ func (b BinaryHeap) isRightChild(index int) bool {
 	return false
 }
 
-func (b BinaryHeap) bubblingUp(i int) {
+func (b *BinaryHeap) bubblingUp(i int) {
 	var parentIndex int
 
 	if b.isRightChild(i) {
@@ -54,23 +54,76 @@ func (b BinaryHeap) bubblingUp(i int) {
 	return
 }
 
+func (b *BinaryHeap) bubblingDown(i int) {
+	lastIndex := len(b.Items) - 1
+	if i == lastIndex {
+		return
+	}
+
+	currentChild := b.Items[i]
+	leftChildIndex := b.getLeftChildIndex(i)
+	rightChildIndex := b.getRightChildIndex(i)
+
+	if leftChildIndex >= lastIndex || rightChildIndex >= lastIndex {
+		return
+	}
+
+	leftChild := b.Items[leftChildIndex]
+	rightChild := b.Items[rightChildIndex]
+
+	if leftChild > rightChild {
+		if currentChild > rightChild {
+			b.Items[i] = rightChild
+			b.Items[rightChildIndex] = currentChild
+			b.bubblingDown(rightChildIndex)
+		}
+	} else {
+		if currentChild > leftChild {
+			b.Items[i] = leftChild
+			b.Items[leftChildIndex] = currentChild
+			b.bubblingDown(leftChildIndex)
+		}
+	}
+}
+
+func (b BinaryHeap) search(item int) *int {
+	for index, node := range b.Items {
+		if node == item {
+			return &index
+		}
+	}
+
+	return nil
+}
+
 func (b *BinaryHeap) Add(item int) {
 	b.Items = append(b.Items, item)
 	b.bubblingUp(len(b.Items) - 1)
 }
 
-func (b BinaryHeap) Remove(item int)  {
-
+func (b *BinaryHeap) Remove(item int) {
+	index := b.search(item)
+	if index == nil {
+		return
+	}
+	lastIndex := len(b.Items) - 1
+	lastChild := b.Items[lastIndex]
+	b.Items[*index] = lastChild
+	b.Items = b.Items[:lastIndex]
+	b.bubblingUp(*index)
 }
 
-func (b BinaryHeap) Poll() {
-
+func (b *BinaryHeap) Poll() {
+	if len(b.Items) == 0 {
+		return
+	}
+	lastIndex := len(b.Items) - 1
+	lastChild := b.Items[lastIndex]
+	b.Items[0] = lastChild
+	b.Items = b.Items[:lastIndex]
+	b.bubblingDown(0)
 }
 
-func (b BinaryHeap) ReturnHeap() []int {
+func (b *BinaryHeap) ReturnHeap() []int {
 	return b.Items
-}
-
-func main() {
-
 }
--- priorityQueue/main_test.go
+++ priorityQueue/main_test.go
@@ -71,137 +71,63 @@ func TestBinaryHeap_Add(t *testing.T) {
 //	}
 //}
 
-//func TestBinaryHeap_getLeftChildIndex(t *testing.T) {
-//	type fields struct {
-//		Items []int
-//	}
-//	type args struct {
-//		parentIndex int
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   int
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			b := &BinaryHeap{
-//				Items: tt.fields.Items,
-//			}
-//			if got := b.getLeftChildIndex(tt.args.parentIndex); got != tt.want {
-//				t.Errorf("getLeftChildIndex() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestBinaryHeap_Poll(t *testing.T) {
+	type fields struct {
+		Items []int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []int
+	}{
+		{name: "Pool first element from queue", fields: fields{
+			Items: []int{1, 5, 1, 8, 6, 2},
+		}, want: []int{1, 5, 2, 8, 6}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := BinaryHeap{
+				Items: tt.fields.Items,
+			}
+			b.Poll()
+			got := b.ReturnHeap()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Add() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-//func TestBinaryHeap_getParentIndexFromLeftChild(t *testing.T) {
-//	type fields struct {
-//		Items []int
-//	}
-//	type args struct {
-//		leftChildIndex int
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   int
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			b := &BinaryHeap{
-//				Items: tt.fields.Items,
-//			}
-//			if got := b.getParentIndexFromLeftChild(tt.args.leftChildIndex); got != tt.want {
-//				t.Errorf("getParentIndexFromLeftChild() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-
-//func TestBinaryHeap_getParentIndexFromRightChild(t *testing.T) {
-//	type fields struct {
-//		Items []int
-//	}
-//	type args struct {
-//		rightChildIndex int
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   int
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			b := &BinaryHeap{
-//				Items: tt.fields.Items,
-//			}
-//			if got := b.getParentIndexFromRightChild(tt.args.rightChildIndex); got != tt.want {
-//				t.Errorf("getParentIndexFromRightChild() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-
-//func TestBinaryHeap_getRightChildIndex(t *testing.T) {
-//	type fields struct {
-//		Items []int
-//	}
-//	type args struct {
-//		parentIndex int
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   int
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			b := &BinaryHeap{
-//				Items: tt.fields.Items,
-//			}
-//			if got := b.getRightChildIndex(tt.args.parentIndex); got != tt.want {
-//				t.Errorf("getRightChildIndex() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-
-//func TestBinaryHeap_isRightChild(t *testing.T) {
-//	type fields struct {
-//		Items []int
-//	}
-//	type args struct {
-//		index int
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			b := BinaryHeap{
-//				Items: tt.fields.Items,
-//			}
-//			if got := b.isRightChild(tt.args.index); got != tt.want {
-//				t.Errorf("isRightChild() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestBinaryHeap_Remove(t *testing.T) {
+	type fields struct {
+		Items []int
+	}
+	type args struct {
+		item int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []int
+	}{
+		{
+			name:   "should remove an item",
+			fields: fields{Items: []int{1, 5, 1, 8, 6, 2}},
+			args:   args{item: 6},
+			want:   []int{1, 2, 1, 8, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &BinaryHeap{
+				Items: tt.fields.Items,
+			}
+			b.Remove(tt.args.item)
+			got := b.ReturnHeap()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Add() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
